fb33263490ff976a6871136813212ed50d1eb9d2
Wizard S <rcvbuf@gmail.com>
2024-11-10 22:00:25 +0800

    fix script error

--- bindings.sh
+++ bindings.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+# 初始化 bindings 变量
 bindings=""
 
-while IFS= read -r line || [ -n "$line" ]; do
-  if [[ ! "$line" =~ ^# ]] && [[ -n "$line" ]]; then
-    name=$(echo "$line" | cut -d '=' -f 1)
-    value=$(echo "$line" | cut -d '=' -f 2-)
-    value=$(echo $value | sed 's/^"\(.*\)"$/\1/')
-    bindings+="--binding ${name}=${value} "
-  fi
-done < .env
+# 检查 .env 文件是否存在
+if [ -f .env ]; then
+    while IFS= read -r line || [ -n "$line" ]; do
+        # 跳过空行和注释行
+        if [[ ! "$line" =~ ^# ]] && [[ -n "$line" ]]; then
+            name=$(echo "$line" | cut -d '=' -f 1)
+            value=$(echo "$line" | cut -d '=' -f 2-)
+            value=$(echo $value | sed 's/^"\(.*\)"$/\1/')
+            bindings+="--binding ${name}=${value} "
+        fi
+    done < .env
 
-bindings=$(echo $bindings | sed 's/[[:space:]]*$//')
+    # 清理末尾的空格
+    bindings=$(echo $bindings | sed 's/[[:space:]]*$//')
+fi
 
+# 如果没有 bindings，输出空字符串
 echo $bindings
